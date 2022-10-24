2511e611af73d9279295b894a59c41c63cc16eeb
davifdepaula <davi.ferreira100@gmail.com>
2022-10-24 16:41:56 -0300

    primeiro bonus

--- script.js
+++ script.js
@@ -1,72 +1,183 @@
-let moves = 0
-const frontCard0 = '<img src = "./imagens/bobrossparrot.gif" alt = "front" />'
-const frontCard1 = '<img src = "./imagens/explodyparrot.gif" alt = "front" />'
-const frontCard2 = '<img src = "./imagens/fiestaparrot.gif" alt = "front" />'
-const frontCard3 = '<img src = "./imagens/metalparrot.gif" alt = "front" />'
-const frontCard4 = '<img src = "./imagens/revertitparrot.gif" alt = "front" />'
-const frontCard5 = '<img src = "./imagens/tripletsparrot.gif" alt = "front" />'
-const frontCard6 = '<img src = "./imagens/unicornparrot.gif" alt = "front" />'
+//let numberCards = Number(prompt("Digite a quantidade de cartas que deseja jogar: "))
+let numberCards = 4
+let plays = 0, seconds = 0
+const timer = document.querySelector('.timer span');
 
-const cards = [frontCard0, frontCard1, frontCard2, frontCard3, frontCard4, frontCard5, frontCard6]
-const backCards = [] 
-const frontCards = []
-const isInFront = []
-
-function startGame(){
-    let j = 0
-    quantity = 4
-    creatBackCards(quantity)
-    creatFrontCards(quantity)
-}
-
-function creatBackCards(quantity){
-    const contente = document.querySelector(".main ul")
-    for(let i = 0; i < quantity ; i++){
-        backCards.push('<li class="card" onclick = "showFront(this)"><img src = "./imagens/back.png" alt = "back" /> </li>')
-    }
-
-    for (i = 0; i < quantity; i++) {
-        contente.innerHTML += backCards[i]
+while (true){
+    if (numberCards % 2 === 0 ){
+        if (numberCards >= 4 && numberCards <= 14){
+            break
+        }
+        else{
+            numberCards = Number(prompt("Digite a quantidade de cartas que deseja jogar: "));
+        }
+    }else{
+        numberCards = Number(prompt("Digite a quantidade de cartas que deseja jogar: "));
     }
 }
 
-function creatFrontCards(quantity){
-    const arr = []
-    console.log(Math.floor(quantity/2))
 
-    while(arr.length < Math.floor(quantity/2)) {
-        let j = Math.floor(Math.random() * cards.length)
-        if(!arr.includes(j)){
-            arr.push(j)
-        }
-    }
+let moves = 0;
+const frontCard0 =
+    '<div class= "front face hideCard"> <img src = "./imagens/bobrossparrot.gif" alt = "bob" /> </div>';
+const frontCard1 =
+    '<div class="front face hideCard"> <img src = "./imagens/explodyparrot.gif" alt = "explody" /> </div>';
+const frontCard2 =
+    '<div class="front face hideCard"><img src = "./imagens/fiestaparrot.gif" alt = "fiesta"  /> </div>';
+const frontCard3 =
+    '<div class="front face hideCard"><img src = "./imagens/metalparrot.gif" alt = "metal" /> </div>';
+const frontCard4 =
+    '<div class="front face hideCard"><img src = "./imagens/revertitparrot.gif" alt = "revert" /> </div>';
+const frontCard5 =
+    '<div class="front face hideCard"><img src = "./imagens/tripletsparrot.gif" alt = "triple" /> </div>';
+const frontCard6 =
+    '<div class="front face hideCard"><img src = "./imagens/unicornparrot.gif" alt = "unicorn" /> </div>';
 
-    while (frontCards.length < quantity){
-        j = Math.floor(Math.random() * arr.length)
-        if (frontCards.includes(arr[j])){
-            frontCards.push(arr[j])
-            arr.splice(j, 1)
-        }
-        else {
-            frontCards.push(arr[j])
-        }
-    }
-    console.log(frontCards)
-    
+const cards = [
+    frontCard0,
+    frontCard1,
+    frontCard2,
+    frontCard3,
+    frontCard4,
+    frontCard5,
+    frontCard6,
+];
+const backCards = '<div class="back face"><img src = "./imagens/back.png" alt = "back" /> </div>'
+const frontCards = [], isInfront = [];
+
+let firstCard = ""
+let secondCard = ""
+
+
+
+function startGame() {
+    creatFrontCards(numberCards);
+    showCards(backCards, frontCards);
+}
+
+function showSeconds(){
+    const content = document.querySelector("header")
+    content.innerHTML += `<div class="showSeconds"> ${seconds} </div>`
 }
 
 
+function creatFrontCards(numberCards) {
+    const arr = [];
+
+    while (arr.length < Math.floor(numberCards / 2)) {
+        let j = Math.floor(Math.random() * cards.length);
+        if (!arr.includes(j)) {
+        arr.push(j);
+        }
+    }
+
+    while (frontCards.length < numberCards) {
+        j = Math.floor(Math.random() * arr.length);
+        if (frontCards.includes(arr[j])) {
+        frontCards.push(arr[j]);
+        arr.splice(j, 1);
+        } else {
+        frontCards.push(arr[j]);
+        }
+    }
+
+    for (let i = 0; i < frontCards.length; i++) {
+        frontCards[i] = cards[frontCards[i]];
+    }
+    }
+
+function showCards(backCards, frontCards) {
+    const content = document.querySelector("main .grid");
+    for (let i = 0; i < numberCards; i++) {
+        content.innerHTML += `<div class='card' onclick = "showFront(this)"> ${frontCards[i]} ${backCards} </div>`;
+    }
+}
+
+function turn(element){
+    element.classList.add("turn")
+}
+
+
+
+function hideCard(element){
+    const front = element
+    const parent = element.parentNode
+    const back = parent.querySelector(".card .back")
+    setTimeout(() => {
+        back.classList.remove("hideCard")
+        front.classList.add("hideCard")
+        setTimeout(() => {
+            firstCard = ""
+            secondCard = ""
+        }, 100)
+    }, 1000)
+}
+
+function endGame(){
+    if(isInfront.length === numberCards/2){
+        setTimeout(() => {
+            alert(`Você ganhou em ${plays} jogadas em ${timer.innerHTML} segundos!`)
+            clearInterval(this.loop)
+        }, 100)
+    }
+    return true
+}
+
+
+function secondClick() {
+    const firstImg = firstCard.getElementsByTagName('img')[0].alt
+    console.log(firstImg)
+    const secondImg = secondCard.getElementsByTagName('img')[0].alt
+    console.log(secondImg)
+    if (firstImg === secondImg) {
+        isInfront.push(firstImg)
+        setTimeout(() => {
+            firstCard = ""
+            secondCard = ""
+        }, 100)
+
+        endGame()
+    }
+    else {
+        hideCard(firstCard)
+        hideCard(secondCard)
+    }
+
+}
 
 function showFront(element) {
-    moves++
-    if(isInFront.includes(element)){
-        frontCards.pop(frontCards.indexOf(element))
+    plays++
+    const front = element.querySelector(".card .front")
+    const back = element.querySelector(".card .back")
+    if (element.classList.contains("hideCard")){
+        return
     }
-    else{
-        console.log("virei de costas")
+    if (firstCard === ""){
+        front.classList.remove("hideCard")
+        back.classList.add("hideCard")
+        firstCard = front
     }
-    console.log(moves)
+
+    else if (secondCard === ""){
+        front.classList.remove("hideCard")
+        back.classList.add("hideCard")
+        secondCard = front
+    }
+
+    if (firstCard !== "" &&  secondCard !== ""){
+        secondClick(firstCard, secondCard)
+    }
+
 }
 
-startGame()
+const startTimer = () => {
+    this.loop = setInterval(() => {
+        const currentTime = Number(timer.innerHTML);
+        timer.innerHTML = currentTime + 1;
+    }, 1000);
+}
 
+window.onload = () => {
+    startTimer();
+    startGame();
+}
